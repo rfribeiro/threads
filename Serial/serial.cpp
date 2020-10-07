936ba74f2413a210f7ceff85c7ed2d054531e163
rfribeiro <rfribeiro@gmail.com>
2020-10-06 22:38:36 -0300

    update serial port

--- Serial/serial.cpp
+++ Serial/serial.cpp
@@ -1,12 +1,15 @@
 #include <windows.h>
 #include <cstring>
 #include <atlstr.h>
+#include <iostream>
 
-bool WriteComPort(CString PortSpecifier, CString data)
+#define PRINTER_PORT "\\\\.\\COM6"
+
+bool WriteComPort(const char* PortSpecifier, char* data, int len)
 {
     DCB dcb = { 0 };
     DWORD byteswritten;
-    HANDLE hPort = CreateFile(
+    HANDLE hPort = CreateFileA(
         PortSpecifier,
         GENERIC_WRITE,
         0,
@@ -17,23 +20,48 @@ bool WriteComPort(CString PortSpecifier, CString data)
     );
     if (!GetCommState(hPort, &dcb))
         return false;
-    dcb.BaudRate = CBR_9600; //9600 Baud
-    dcb.ByteSize = 8; //8 data bits
-    dcb.Parity = NOPARITY; //no parity
-    dcb.StopBits = ONESTOPBIT; //1 stop
+    dcb.BaudRate = CBR_9600;     //9600 Baud
+    dcb.ByteSize = 8;            //8 data bits
+    dcb.Parity = NOPARITY;      //no parity
+    dcb.StopBits = ONESTOPBIT;  //1 stop
     if (!SetCommState(hPort, &dcb))
         return false;
-    bool retVal = WriteFile(hPort, data, 1, &byteswritten, NULL);
+
+    bool retVal = WriteFile(hPort, data, len, &byteswritten, NULL);
+
+    if (len == byteswritten)
+    {
+        std::cout << "command write with success" << std::endl;
+    }
     //ReadFile()
     CloseHandle(hPort); //close the handle
     return retVal;
 }
 
+
 int main() {
        
-    CString data;
-    data.Format(_T("%X"), 0xff);
-    WriteComPort(L"\\\\.\\COM3", "teste");
+    char data[20];
+
+    // init default
+    data[0] = 0x1B;
+    data[1] = '@';
+
+    // set mode (BEMA)
+    data[2] = 0x1D;
+    data[3] = 0xf9;
+    data[4] = 0x35;
+    data[5] = 0x00;
+    WriteComPort(PRINTER_PORT, data, 6);
+
+    // send data
+    strcpy_s(data, "teste impressão");
+    WriteComPort(PRINTER_PORT, data, 15);
+
+    // cut paper
+    data[0] = 0x1B;
+    data[1] = 0x69;
+    WriteComPort(PRINTER_PORT, data, 2);
 
     return 0;
 }
